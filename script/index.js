7fd135854cac7d7b967c57f89e28c998ad97c318
Harshit <dev.harshit19@gmail.com>
2020-10-05 22:58:09 +0530

    assignment-4

--- script/index.js
+++ script/index.js
@@ -1,4 +1,16 @@
 $(document).ready(function () {
+  //for login modal
+  $("#loginButton").click(function () {
+    $("#loginModal").modal("show");
+  });
+  $("#reserveButton").click(function () {
+    $("#reservationModal").modal("show");
+  });
+  $("#loginClose").click(function () {
+    $("#loginModal").modal("hide");
+  });
+
+  //for carousel play and pause button
   $("#myCarousel").carousel({ interval: 2000 });
   $("#carouselButton").click(function () {
     if ($("#carouselButton").children("span").hasClass("fa-pause")) {
