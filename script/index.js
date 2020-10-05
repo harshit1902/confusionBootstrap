d60b322f7a730296e1448e652f0bf38c8ef26b4e
Harshit <dev.harshit19@gmail.com>
2020-10-05 16:05:12 +0530

    single Play and Pause

--- script/index.js
+++ script/index.js
@@ -1,9 +1,15 @@
 $(document).ready(function () {
   $("#myCarousel").carousel({ interval: 2000 });
-  $("#carousel-pause").click(function () {
-    $("#myCarousel").carousel("pause");
-  });
-  $("#carousel-play").click(function () {
+  $("#carouselButton").click(function () {
+    if ($("#carouselButton").children("span").hasClass("fa-pause")) {
+      $("#myCarousel").carousel("pause");
+      $("#carouselButton").children("span").removeClass("fa-pause");
+      $("#carouselButton").children("span").addClass("fa-play");
+      return;
+    }
     $("#myCarousel").carousel("cycle");
+    $("#carouselButton").children("span").removeClass("fa-play");
+    $("#carouselButton").children("span").addClass("fa-pause");
+    return;
   });
 });
